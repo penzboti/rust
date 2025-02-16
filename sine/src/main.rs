b0bfa0ccd3cfc7aacbc68381acf9770f682e5557
penzboti <penzboti@gmail.com>
2025-02-16 22:07:21 +0100

    Sine: added rainbow color, rearranged some code

--- sine/src/main.rs
+++ sine/src/main.rs
@@ -1,67 +1,27 @@
-const SLEEP_MS: u64 = 10;
-const PRINT_I: bool = false;
-const HEIGHT: u32 = 31;
-
 // options setup
 struct Options {
     speed: u64,
     numbers: bool,
+    color: bool,
     width: u32,
     length: u32,
 }
 impl Options {
     fn new(size: u32) -> Options {
+        let speed_ms: u64 = 10;
+        let height: u32 = 31; // this is set to pi*10
         Options {
-            speed: SLEEP_MS,
-            numbers: PRINT_I,
-            width: size / 8,
-            length: HEIGHT,
+            speed: speed_ms,
+            numbers: false,
+            color: false,
+            width: size,
+            length: height,
         }
     }
 }
 
-// toggling cursor
-// https://stackoverflow.com/questions/55578765/is-there-possible-way-to-hide-cursor-inter-run-time-in-console-in-fortran
-struct Cursor;
-impl Drop for Cursor {
-    fn drop(&mut self) {
-        println!("exiting...");
-        // re-enables cursor
-        print!("\x1B[?25h");
-        // resets terminal color
-        // print!("\x1B[0m");
-    }
-}
-impl Cursor {
-    fn new() -> Cursor {
-        // TODO: color
-        // set color: requires TRUECOLOR terminal
-        // bg is 48;
-        // print!("\x1b[38;2;255;255;0m");
-
-        // disable cursor
-        print!("\x1B[?25l");
-        return Cursor;
-    }
-}
-
-use std::f32::consts::PI;
-// for ctrl-c
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-
-fn getsine(x: f32) -> f32 {
-    return f32::sin(x);
-}
-
-fn main() {
-    // terminal size stuff
-    let size = termsize::get().unwrap();
-    let centerx = size.cols / 2 - 1;
-
-    let mut options = Options::new(size.cols as u32);
-    // command line arguments
-    let mut args: Vec<String> = std::env::args().collect();
+fn handle_args(mut args: Vec<String>, size: u32) -> Option<Options> {
+    let mut options = Options::new(size);
     args.remove(0);
     loop {
         if args.len() == 0 {
@@ -74,19 +34,26 @@ fn main() {
             continue;
         }
         if arg == "-h" || arg == "--help" {
-            let msg = "Sine wave
--h --help  ->  display this help message
--n --numbers  ->  display the current iteration
--w --width <num>  ->  set the amount of characters in one direction. The default is 25% of the screen width.
--l --length <num>  ->  set the iteration length for one arc.";
+            let msg = "Sine wave in the middle of your screen.
+-h, --help\t\tDisplay this help message.
+-n, --numbers\t\tDisplay the current iteration.
+-c, --colo[u]r\t\tEnable rainbow coloring for the text.
+-w, --width <num>\tSet the amount of characters in one direction. The default is 1/8th of the screen width.
+It will crash if you set it to more than half of your screen length.
+-l, --length <num>\tSet the iteration length for one arc. The default is 31 (pi*10).";
             for line in msg.split('\n') {
                 println!("{}", line);
             }
-            return;
+            return None;
+        }
+        if arg == "-c" || arg == "--color" || arg == "--colour" {
+            options.color = true;
+            continue;
         }
 
-        let val = args[0].clone().parse().unwrap();
+        let stringval = args[0].clone();
         args.remove(0);
+        let val = stringval.parse().unwrap_or(1);
         if arg == "-w" || arg == "--width" {
             options.width = val;
         }
@@ -97,6 +64,50 @@ fn main() {
             options.speed = val as u64;
         }
     }
+    return Some(options);
+}
+
+// toggling cursor
+// https://stackoverflow.com/questions/55578765/is-there-possible-way-to-hide-cursor-inter-run-time-in-console-in-fortran
+struct Cursor;
+impl Drop for Cursor {
+    fn drop(&mut self) {
+        // resets terminal color
+        print!("\x1B[0m");
+        println!("exiting...");
+        // re-enables cursor
+        print!("\x1B[?25h");
+    }
+}
+impl Cursor {
+    fn new() -> Cursor {
+        // disable cursor
+        print!("\x1B[?25l");
+        return Cursor;
+    }
+}
+
+// for ctrl-c
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+use std::f32::consts::PI;
+fn getsine(x: f32) -> f32 {
+    return f32::sin(x);
+}
+
+fn main() {
+    // terminal size stuff
+    let size = termsize::get().unwrap();
+    let centerx = size.cols / 2 - 1;
+
+    // command line arguments
+    let args: Vec<String> = std::env::args().collect();
+    let options = handle_args(args, size.cols as u32 / 8 as u32);
+    if options.is_none() {
+        return;
+    }
+    let options = options.unwrap();
 
     // ctrl-c handling
     let running = Arc::new(AtomicBool::new(true));
@@ -111,10 +122,12 @@ fn main() {
 
     // main loop
     let mut i: u16 = 0;
+    let mut c: i32 = 0;
     while running.load(Ordering::SeqCst) {
         let x = getsine(i as f32 / (options.length as f32 / PI)) * options.width as f32;
         let offset_before = x.round() as usize;
         let offset_after = -x.round() as usize;
+
         // print a pretty $i
         let si = if options.numbers {
             print!("{}", i);
@@ -122,6 +135,25 @@ fn main() {
         } else {
             0
         };
+
+        // change terminal color
+        if options.color {
+            if c > 359 {
+                c = 0;
+            }
+
+            let (r, g, b) = hsl::HSL {
+                h: c as f64,
+                s: 1_f64,
+                l: 0.5_f64,
+            }
+            .to_rgb();
+            c += 1;
+
+            // just so you know, the code 48; sets the background color
+            // requires TRUECOLOR terminal
+            print!("\x1b[38;2;{r};{g};{b}m");
+        }
 
         for _ in 0..centerx as usize - offset_after - si {
             print!(" ");
